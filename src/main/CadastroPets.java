bc5bbc61cec0e4b18a87111c81617fd528b857af
Murilo Luiz <munofoente@gmail.com>
2026-01-19 21:09:28 -0300

    Exibição dos menus

--- src/main/CadastroPets.java
+++ src/main/CadastroPets.java
@@ -1,10 +1,20 @@
 package main;
 
 import java.io.BufferedReader;
-import java.io.File;
 import java.io.FileReader;
+import java.util.Scanner;
 
-public class Program {
+public class CadastroPets {
+
+    public static void lerMenu(){
+        System.out.println(
+                "1. Cadastrar um novo pet\n" +
+                        "2. Alterar os dados do pet cadastrado\n" +
+                        "3. Deletar um pet cadastrado\n" +
+                        "4. Listar todos os pets cadastrados\n" +
+                        "5. Listar pets por algum critério (idade, nome, raça)\n" +
+                        "6. Sair\n");
+    }
 
     public static void lerPerguntas(){
 
@@ -21,8 +31,26 @@ public class Program {
     }
 
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n;
+        try{
+            do {
+                lerMenu();
+                System.out.print("Insira a sua opção: ");
+                n = sc.nextInt();
+            } while (n <= 0);
 
-        
+            switch (n){
+                case 1:
+                    lerPerguntas();
+                    break;
+            }
+
+        } catch (Exception e) {
+            System.out.println("Erro: Entrada inválida!");
+        }
+
+
+        sc.close();
     }
-
 }
